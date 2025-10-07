e28309e4d5435e6a77b8b3362bca692d93b68bb7
Renujan <roshanrenujan20020426@gmail.com>
2025-10-07 14:46:57 +0530

    created profile model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
-# Create your models here.
+class Profile(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    score = models.IntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        # Hash the password before saving
+        if not self.pk:  # only hash when creating new user
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.username
