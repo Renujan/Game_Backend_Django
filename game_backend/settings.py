cfc164fc1393d9944422e37e98fbe6e38fd9111c
Renujan <roshanrenujan20020426@gmail.com>
2025-11-11 15:41:18 +0530

    smtp implemented

--- game_backend/settings.py
+++ game_backend/settings.py
@@ -46,8 +46,13 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'demonslayer008.r@gmail.com'       # your Gmail
+EMAIL_HOST_PASSWORD = 'sxwafftzqljkvseg'    # App password (see below)
+DEFAULT_FROM_EMAIL = 'Banana Game <demonslayer008.r@gmail.com>'
 
 
 MIDDLEWARE = [
