8be62a6a59fed4c521332f69cd826e2806695dfd
Renujan <roshanrenujan20020426@gmail.com>
2025-11-11 15:27:59 +0530

    tested otp system

--- game_backend/settings.py
+++ game_backend/settings.py
@@ -46,6 +46,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
 
 
 MIDDLEWARE = [
